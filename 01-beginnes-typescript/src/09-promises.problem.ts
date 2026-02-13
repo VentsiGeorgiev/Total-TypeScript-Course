50bd039399e5a4b9524d9c8093d7661ecaac2b61
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-02-13 18:04:40 +0100

    Solve Beginner TypeScript workshop exercises

--- 01-beginnes-typescript/src/09-promises.problem.ts
+++ 01-beginnes-typescript/src/09-promises.problem.ts
@@ -1,20 +1,37 @@
 interface LukeSkywalker {
-  name: string;
-  height: string;
-  mass: string;
-  hair_color: string;
-  skin_color: string;
-  eye_color: string;
-  birth_year: string;
-  gender: string;
+  name: string
+  height: string
+  mass: string
+  hair_color: string
+  skin_color: string
+  eye_color: string
+  birth_year: string
+  gender: string
 }
 
-export const fetchLukeSkywalker = async (): LukeSkywalker => {
-  const data = await fetch("https://swapi.py4e.com/api/people/1").then(
-    (res) => {
-      return res.json();
-    }
-  );
+// # Solution 3
+export const fetchLukeSkywalker = async () => {
+  const response = await fetch('https://swapi.py4e.com/api/people/1')
+  const data = await response.json()
+  return data.json() as LukeSkywalker
+}
+// # Solution 2
+// export const fetchLukeSkywalker = async () => {
+//   const data: LukeSkywalker = await fetch(
+//     'https://swapi.py4e.com/api/people/1',
+//   ).then((res) => {
+//     return res.json()
+//   })
 
-  return data;
-};
+//   return data
+// }
+// # Solution1
+// export const fetchLukeSkywalker = async (): Promise<LukeSkywalker> => {
+//   const data = await fetch('https://swapi.py4e.com/api/people/1').then(
+//     (res) => {
+//       return res.json()
+//     },
+//   )
+
+//   return data
+// }
--- 01-beginnes-typescript/src/17-function-types.problem.ts
+++ 01-beginnes-typescript/src/17-function-types.problem.ts
@@ -1,20 +1,23 @@
-import { Equal, Expect } from "./helpers/type-utils";
+import { Equal, Expect } from './helpers/type-utils'
 
 /**
  * How do we type onFocusChange?
  */
-const addListener = (onFocusChange: unknown) => {
-  window.addEventListener("focus", () => {
-    onFocusChange(true);
-  });
 
-  window.addEventListener("blur", () => {
-    onFocusChange(false);
-  });
-};
+type Listener = (isFocused: boolean) => void
+
+const addListener = (onFocusChange: Listener) => {
+  window.addEventListener('focus', () => {
+    onFocusChange(true)
+  })
+
+  window.addEventListener('blur', () => {
+    onFocusChange(false)
+  })
+}
 
 addListener((isFocused) => {
-  console.log({ isFocused });
+  console.log({ isFocused })
 
-  type tests = [Expect<Equal<typeof isFocused, boolean>>];
-});
+  type tests = [Expect<Equal<typeof isFocused, boolean>>]
+})
