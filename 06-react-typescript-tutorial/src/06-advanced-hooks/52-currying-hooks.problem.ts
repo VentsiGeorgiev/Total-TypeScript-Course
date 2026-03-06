b6c1d6b4f2e2437d656ad785b31c0e75d5082ab7
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-03-06 19:20:50 +0100

    solve advanced hooks

--- 06-react-typescript-tutorial/src/06-advanced-hooks/52-currying-hooks.problem.ts
+++ 06-react-typescript-tutorial/src/06-advanced-hooks/52-currying-hooks.problem.ts
@@ -8,14 +8,13 @@ const useCustomState = <TValue>(initial: TValue) => {
     value,
     set,
     /**
-     * Here, we're returning a hook from our initial hook!
-     * This is a great way to compose behaviour.
-     *
-     * BUT - useComputed takes in a function which can return
-     * any type. We want to make sure that the type of the
-     * thing returned is inferred properly.
+     * We can use a generic _inline_ here to ensure
+     * this all still works.
      */
-    useComputed: (factory: (value: any) => any, deps?: DependencyList) => {
+    useComputed: <TComputed>(
+      factory: (value: TValue) => TComputed,
+      deps?: DependencyList,
+    ) => {
       return useMemo(() => {
         return factory(value);
       }, [value, ...(deps || [])]);
@@ -26,11 +25,6 @@ const useCustomState = <TValue>(initial: TValue) => {
 const Component = () => {
   const arrayOfNums = useCustomState([1, 2, 3, 4, 5, 6, 7, 8]);
 
-  /**
-   * Currently, this is typed as any. How do we type it
-   * based on the return type of the function passed to
-   * useComputed?
-   */
   const reversedAsString = arrayOfNums.useComputed((nums) =>
     Array.from(nums).reverse().map(String),
   );
